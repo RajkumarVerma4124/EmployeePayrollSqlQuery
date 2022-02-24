5f213af5b592a5bf91fbb40bdedfbb7cca02d399
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-24 10:25:10 +0530

    [Raj] Added:- The table employee_payroll by using create table

--- EmployeePayRoll.sql
+++ EmployeePayRoll.sql
@@ -3,3 +3,11 @@
 ---------Create DB (UC1)
 create database PayRoll_Service;
 use PayRoll_Service;
+
+---------Create Table (UC2)
+CREATE TABLE Employee_Payroll (
+    Id int identity(1,1) primary key,
+    Name varchar(255) not null,
+    Salary float,
+    StartDate date,
+); 
