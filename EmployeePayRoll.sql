869eb02f5dfcbc506b1671bbc3ab03b75da74591
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-24 10:22:02 +0530

    [Raj] Added:- The db payroll service by using create database

--- EmployeePayRoll.sql
+++ EmployeePayRoll.sql
@@ -1,2 +1,5 @@
 -----------------------------Welcome To The Employee PayRoll Service DataBase---------------------------------
+
+---------Create DB (UC1)
 create database PayRoll_Service;
+use PayRoll_Service;
